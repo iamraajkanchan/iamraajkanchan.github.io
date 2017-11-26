95d169bad46c7d556fa2f98ed9fb84acebbcaaf6
iamraajkanchan <iamraajkanchan@gmail.com>
2017-11-26 20:51:29 +0530

    Updated on 26.11.2017
    Updated all scripts in html, css and javascripts on index and product_photography page.

--- js/photography-script.js
+++ js/photography-script.js
@@ -1,4 +1,26 @@
-$(window).scroll(function(){
+$(function(){
+	$('.product-photography-child').each(function(i){
+			setTimeout(function(){
+			   $('.product-photography-child').eq(i).addClass('is-showing');
+			   }, 500 * (i+1));
+			});
+	var myMedNavbar = $('.med-navbar');
+	var myNavToggleButton = $('.med-navbar-button');
+	var i = 0;
+	myNavToggleButton.click(function(){
+			if(i == 0){
+			myMedNavbar.css({"transform": "translateY(10px)"});
+			i = 1;
+		}
+		else{
+			myMedNavbar.css({"transform": "translateY(-500px)"});
+			i = 0;
+		}
+	});
+});
+
+
+/*$(window).scroll(function(){
 	var wScroll = $(this).scrollTop();
 	if(wScroll > $('.product-photography-child').offset().top - ($(window).height()/ 5)){
 		$('.product-photography-child').each(function(i){
--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
-jQuery(function(){
+$(function(){
 	// initializing the first div to class 'top'
+	//$('.home-logo-container').css({left: "18%", opacity: 1.0});
+	$('.home-logo-container').css({opacity: 1.0});
+	$('.home-menu-slide-navbar').css({opacity: 1.0});
+	$('.home-content').css({opacity: 1.0});
 	$('.home-fade-slide-container div:first').addClass('top');
 	//function to alter the image index by changing the class
     var change = function (){
@@ -19,7 +23,7 @@ jQuery(function(){
 		          	curr.css({opacity: 1.0});
   		        });
      }else{
-				// pick the next image
+				// pick the next div
 				// set the opacity to 0
 			    // then use animate to make it appear
 				// above the top image
@@ -32,9 +36,20 @@ jQuery(function(){
                  });
 		    }
      }
-
-
 // repeat the function execution for every 5 secs
     setInterval(change, 5000);
+	var myMedNavbar = $('.med-navbar');
+	var myNavToggleButton = $('.med-navbar-button');
+	var i = 0;
+	myNavToggleButton.click(function(){
+			if(i == 0){
+			myMedNavbar.css({"transform": "translateY(50px)"});
+			i = 1;
+		}
+		else{
+			myMedNavbar.css({"transform": "translateY(-500px)"});
+			i = 0;
+		}
+	});
 });
 
